7444b4c8c9fc76a6ce41dd09ae30b486d3e0932d
David Gohel <david.gohel@ardata.fr>
2015-08-27 01:39:26 +0200

    doc

--- examples/geom_segment_interactive.R
+++ examples/geom_segment_interactive.R
@@ -2,7 +2,8 @@ counts <- as.data.frame(table(x = rpois(100,5)))
 counts$x <- as.numeric( as.character(counts$x) )
 counts$xlab <- paste0("bar",as.character(counts$x) )
 
-gg_segment_1 <- ggplot(data = counts, aes(x = x, y = Freq, yend = 0, xend = x, tooltips = xlab ) ) + 
+gg_segment_1 <- ggplot(data = counts, aes(x = x, y = Freq, 
+			yend = 0, xend = x, tooltips = xlab ) ) + 
 	geom_segment_interactive( size = I(10))
 
 dataset = data.frame(x=c(1,2,5,6,8), 
@@ -11,7 +12,8 @@ dataset = data.frame(x=c(1,2,5,6,8),
 		vy=c(0.2,1.3,1.7,0.8,1.4), 
 		labs = paste0("Lab", 1:5))
 gg_segment_2 = ggplot() + 
-		geom_segment_interactive(data=dataset, mapping=aes(x=x, y=y, xend=x+vx, yend=y+vy, tooltips = labs), 
+		geom_segment_interactive(data=dataset, mapping=aes(x=x, y=y, 
+					xend=x+vx, yend=y+vy, tooltips = labs), 
 				arrow=grid::arrow(length = grid::unit(0.03, "npc")), 
 				size=2, color="blue") + 
 		geom_point(data=dataset, mapping=aes(x=x, y=y), size=4, shape=21, fill="white") 
