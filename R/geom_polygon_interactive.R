00a7b9a5c90485a69cf64f0fd1e692a443914ff5
David Gohel <david.gohel@ardata.fr>
2016-01-25 01:39:24 +0100

    geom_map_interactive

--- R/geom_polygon_interactive.R
+++ R/geom_polygon_interactive.R
@@ -68,3 +68,88 @@ GeomInteractivePolygon <- ggproto("GeomInteractivePolygon", Geom,
 
 		draw_key = draw_key_polygon
 )
+
+
+
+#' @importFrom ggplot2 PositionIdentity
+#' @title interactive polygons from a reference map.
+#'
+#' @description
+#' tooltips can be displayed when mouse is over segments, on click actions and
+#' double click actions can be set with javascript instructions.
+#'
+#' @param map Data frame that contains the map coordinates. See \code{\link[ggplot2]{geom_map}}.
+#' @inheritParams geom_point_interactive
+#' @examples
+#' # add interactive maps to a ggplot -------
+#' @example examples/geom_map_interactive.R
+#' @seealso \code{\link{ggiraph}}
+#' @export
+geom_map_interactive <- function(mapping = NULL, data = NULL, map, stat = "identity",
+                                 na.rm = FALSE, show.legend = NA, inherit.aes = TRUE, ...) {
+  # Get map input into correct form
+  stopifnot(is.data.frame(map))
+  if (!is.null(map$lat)) map$y <- map$lat
+  if (!is.null(map$long)) map$x <- map$long
+  if (!is.null(map$region)) map$id <- map$region
+  stopifnot(all(c("x", "y", "id") %in% names(map)))
+
+  layer(
+    data = data,
+    mapping = mapping,
+    stat = stat,
+    geom = GeomInteractiveMap,
+    position = PositionIdentity,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(
+      map = map,
+      na.rm = na.rm,
+      ...
+    )
+  )
+}
+
+GeomInteractiveMap <- ggproto(
+  "GeomInteractiveMap",
+  GeomInteractivePolygon,
+  draw_panel = function(data, panel_scales, coord, map) {
+    # Only use matching data and map ids
+    common <- intersect(data$map_id, map$id)
+    data <- data[data$map_id %in% common, , drop = FALSE]
+    map <- map[map$id %in% common, , drop = FALSE]
+
+    # Munch, then set up id variable for polygonGrob -
+    # must be sequential integers
+    coords <- coord_munch(coord, map, panel_scales)
+    if (!is.null(coords$group))
+      coords$group <- coords$group
+    else
+      coords$group <- coords$id
+    grob_id <- match(coords$group, unique(coords$group))
+
+    # Align data with map
+    data_rows <- match(coords$id[!duplicated(grob_id)], data$map_id)
+    data <- data[data_rows, , drop = FALSE]
+    run_l <- rle(grob_id)
+
+    args <- list( x = coords$x, y = coords$y,
+      default.units = "native", id = grob_id,
+      gp = gpar(
+        col = data$colour,
+        fill = alpha(data$fill, data$alpha),
+        lwd = data$size * .pt
+      )
+    )
+
+    if (!is.null(data$tooltip))
+      args$tooltip <- unlist(mapply(rep, data$tooltip, run_l$lengths))
+    if (!is.null(data$onclick))
+      args$onclick <- unlist(mapply(rep, data$onclick, run_l$lengths))
+    if (!is.null(data$data_id))
+      args$data_id <- unlist(mapply(rep, data$data_id, run_l$lengths))
+    do.call(interactivePolygonGrob, args)
+  },
+
+  required_aes = c("map_id")
+)
---
+++ examples/geom_map_interactive.R
@@ -0,0 +1,37 @@
+crimes <- data.frame(state = tolower(rownames(USArrests)), USArrests)
+
+# create tooltips and onclick events
+states_ <- sprintf("<p>%s</p>",
+                   as.character(crimes$state) )
+table_ <- paste0(
+  "<table><tr><td>UrbanPop</td>",
+  sprintf("<td>%.0f</td>", crimes$UrbanPop),
+  "</tr><tr>",
+  "<td>Assault</td>",
+  sprintf("<td>%.0f</td>", crimes$Assault),
+  "</tr></table>"
+)
+
+onclick <- sprintf(
+  "function() {window.open('%s%s')}",
+  "http://en.wikipedia.org/wiki/",
+  as.character(crimes$state)
+)
+
+
+crimes$labs <- paste0(states_, table_)
+crimes$onclick = onclick
+
+if (require("maps") ) {
+  states_map <- map_data("state")
+  gg_map <- ggplot(crimes, aes(map_id = state))
+  gg_map <- gg_map + geom_map_interactive(aes(
+                  fill = Murder,
+                  tooltip = labs,
+                  onclick = onclick
+                ),
+                map = states_map) +
+    expand_limits(x = states_map$long, y = states_map$lat)
+  ggiraph(fun=print, x = gg_map, width = 8, height = 6)
+}
+
