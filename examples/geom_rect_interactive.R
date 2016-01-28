5280107fb853a8fecb403fcd14f71bb75d2e0d59
David Gohel <david.gohel@ardata.fr>
2016-01-28 01:37:09 +0100

    cleaning

--- examples/geom_rect_interactive.R
+++ examples/geom_rect_interactive.R
@@ -1,35 +1,25 @@
-dataset <- data.frame(
-	x = sample(10, 20, replace = TRUE),
-	y = sample(10, 20, replace = TRUE), 
-	id = letters[1:20]
-)
-gg_rect_1 = ggplot(dataset, aes(xmin = x, xmax = x + 1, 
-		ymin = y, ymax = y + 2, tooltip = id)) +
-	geom_rect_interactive()
-
-dataset = data.frame( x1 = c(1, 3, 1, 5, 4), 
-	x2 = c(2, 4, 3, 6, 6), 
-	y1 = c( 1, 1, 4, 1, 3), 
-	y2 = c( 2, 2, 5, 3, 5), 
-	t = c( 'a', 'a', 'a', 'b', 'b'), 
-	r = c( 1, 2, 3, 4, 5), 
+dataset = data.frame( x1 = c(1, 3, 1, 5, 4),
+	x2 = c(2, 4, 3, 6, 6),
+	y1 = c( 1, 1, 4, 1, 3),
+	y2 = c( 2, 2, 5, 3, 5),
+	t = c( 'a', 'a', 'a', 'b', 'b'),
+	r = c( 1, 2, 3, 4, 5),
 	tooltip = c("ID 1", "ID 2", "ID 3", "ID 4", "ID 5"),
 	uid = c("ID 1", "ID 2", "ID 3", "ID 4", "ID 5"),
 	oc = rep("function() {alert(this.getAttribute('data-id'))}", 5)
 )
 
-gg_rect_2 = ggplot() + 
-	scale_x_continuous(name="x") + 
+gg_rect = ggplot() +
+	scale_x_continuous(name="x") +
 	scale_y_continuous(name="y") +
-	geom_rect_interactive(data=dataset, 
-		mapping = aes(xmin = x1, xmax = x2, 
-			ymin = y1, ymax = y2, fill = t, 
-			tooltip = tooltip, onclick = oc, data_id = uid ), 
+	geom_rect_interactive(data=dataset,
+		mapping = aes(xmin = x1, xmax = x2,
+			ymin = y1, ymax = y2, fill = t,
+			tooltip = tooltip, onclick = oc, data_id = uid ),
 		color="black", alpha=0.5) +
-	geom_text(data=dataset, 
-			aes(x = x1 + ( x2 - x1 ) / 2, y = y1 + ( y2 - y1 ) / 2, 
-					label = r ), 
-		size = 4 ) 
+	geom_text(data=dataset,
+			aes(x = x1 + ( x2 - x1 ) / 2, y = y1 + ( y2 - y1 ) / 2,
+					label = r ),
+		size = 4 )
 
-ggiraph(fun=print, x = gg_rect_1)
-ggiraph(fun=print, x = gg_rect_2)
+ggiraph(fun=print, x = gg_rect)
--- examples/geom_text_interactive.R
+++ examples/geom_text_interactive.R
@@ -2,18 +2,17 @@
 dataset = mtcars
 dataset$label = row.names(mtcars)
 
-dataset$tooltip = paste0( "<h5>", row.names(mtcars) ,"</h5>",
-                          "<ul><li>cyl:", dataset$cyl, "</li>",
-       "<li>gear:", dataset$gear, "</li>",
-       "<li>carb:", dataset$carb, "</li></ul>")
+dataset$tooltip = paste0( "cyl: ", dataset$cyl, "<br/>",
+       "gear: ", dataset$gear, "<br/>",
+       "carb: ", dataset$carb)
 
 ## the plot
 gg_text = ggplot(dataset,
                  aes(x = mpg, y = wt, label = label,
                      color = qsec,
-                     tooltip = tooltip) ) +
+                     tooltip = tooltip, data_id = label ) ) +
   geom_text_interactive() +
   coord_cartesian(xlim = c(0,50))
 
 ## display the plot
-ggiraph(fun=print, x = gg_text)
+ggiraph(fun=print, x = gg_text, hover_css = "{fill:#FF4C3B;font-style:italic;}")
