5f6bd4440c35919e9d0200ea7b6f9d8b72daf9fc
dgohel <david.gohel@ardata.fr>
2017-08-30 18:13:28 +0200

    minor

--- inst/shiny/iris/ui.R
+++ inst/shiny/iris/ui.R
@@ -2,7 +2,7 @@ library(ggiraph)
 shinyUI(fluidPage(
   fluidRow(
     column(
-      width=12,
+      width=6,offset = 3,
       textInput("selpoint", label = "Selected species"),
       ggiraphOutput("plot")
     )
