e68edf62bb23516ef971b34e12e57e654585a2c8
davidgohel <david.gohel@ardata.fr>
2018-05-04 18:03:42 +0200

    fix PR

--- examples/geom_vline_interactive.R
+++ examples/geom_vline_interactive.R
@@ -2,17 +2,24 @@ library(ggplot2)
 
 
 g1 <- ggplot(diamonds, aes(carat)) +
-      geom_histogram()
+  geom_histogram()
 
-gg_vline1 <- g1 + geom_vline_interactive(aes(xintercept = mean(carat), tooltip = round(mean(carat),2), data_id = carat))
+gg_vline1 <- g1 + geom_vline_interactive(
+  aes(xintercept = mean(carat),
+      tooltip = round(mean(carat), 2),
+      data_id = carat))
 
 dataset <- data.frame(x = rnorm(100))
 
-dataset$clickjs = rep(paste0("alert(\"",round(mean(dataset$x), 2), "\")"), 100)
+dataset$clickjs <- rep(paste0("alert(\"",
+                              round(mean(dataset$x), 2), "\")"), 100)
 
 g2 <- ggplot(dataset, aes(x)) +
-      geom_density(fill = "#000000", alpha = 0.7)
-gg_vline2 <- g2 + geom_vline_interactive(aes(xintercept = mean(x), tooltip = round(mean(x), 2), data_id = x, onclick = clickjs), color = "white")
+  geom_density(fill = "#000000", alpha = 0.7)
+gg_vline2 <- g2 + geom_vline_interactive(
+  aes(xintercept = mean(x), tooltip = round(mean(x), 2),
+      data_id = x, onclick = clickjs), color = "white")
 
 ggiraph(code = print(gg_vline1))
-ggiraph(code = print(gg_vline2), hover_css = "cursor:pointer;fill:orange;stroke:orange;")
+ggiraph(code = print(gg_vline2),
+        hover_css = "cursor:pointer;fill:orange;stroke:orange;")
