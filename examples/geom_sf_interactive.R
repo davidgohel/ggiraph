28c482ce64fbd5257edac9ba273d84dcbf02658a
davidgohel <david.gohel@ardata.fr>
2018-06-09 18:32:42 +0200

    geom_sf_interactive

---
+++ examples/geom_sf_interactive.R
@@ -0,0 +1,24 @@
+library(ggplot2)
+library(ggiraph)
+
+## original code: see section examples of ggplot2::geom_sf help file
+if (requireNamespace("sf", quietly = TRUE)) {
+  nc <- sf::st_read(system.file("shape/nc.shp", package = "sf"), quiet = TRUE)
+  gg <- ggplot(nc) +
+    geom_sf_interactive(aes(fill = AREA, tooltip = NAME, data_id = NAME))
+  ggiraph( ggobj = gg)
+
+  nc_3857 <- sf::st_transform(nc, "+init=epsg:3857")
+
+  # Unfortunately if you plot other types of feature you'll need to use
+  # show.legend to tell ggplot2 what type of legend to use
+  nc_3857$mid <- sf::st_centroid(nc_3857$geometry)
+  gg <- ggplot(nc_3857) +
+    geom_sf(colour = "white") +
+    geom_sf_interactive(aes(geometry = mid,
+        size = AREA, tooltip = NAME, data_id = NAME),
+      show.legend = "point")
+  ggiraph( ggobj = gg)
+
+}
+
