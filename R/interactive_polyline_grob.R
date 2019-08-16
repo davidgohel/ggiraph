ff544707727de8d8e412c7c1264827a80273f751
David Gohel <david.gohel@ardata.fr>
2019-08-16 15:24:29 +0200

    add drawDetails.interactive_lines_grob

--- R/interactive_polyline_grob.R
+++ R/interactive_polyline_grob.R
@@ -59,3 +59,20 @@ drawDetails.interactive_polyline_grob <- function(x, recording) {
   }
   invisible()
 }
+
+#' @export
+#' @title interactive_lines_grob drawing
+#' @description draw an interactive_lines_grob
+#' @inheritParams grid::drawDetails
+drawDetails.interactive_lines_grob <- function(x, recording) {
+  dsvg_tracer_on()
+  do.call(grid.lines, x[grob_argnames(x = x, grob = grid::linesGrob)])
+  ids <- dsvg_tracer_off()
+  if (length(ids) > 0) {
+    if (is.null(x$id) && is.null(x$id.lengths))
+      x$id <- rep(1, length(x$x))
+    .w = c(TRUE, x$id[-1] != x$id[-length(x$id)])
+    interactive_attr_toxml(x = x, ids = ids, rows = .w)
+  }
+  invisible()
+}
