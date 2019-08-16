a21328dbe033cd286fe33ea5ed8ff7721fd10989
David Gohel <david.gohel@ardata.fr>
2019-08-16 18:11:19 +0200

    minor

--- R/00_scale.R
+++ R/00_scale.R
@@ -51,6 +51,15 @@ scale_fill_manual_interactive <- function(..., values, data_id = NULL, onclick =
 
 #' @export
 #' @importFrom scales seq_gradient_pal
+#' @title Create your own interactive gradient scale
+#' @description The scale is based on \code{\link[ggplot2]{scale_colour_gradient}}.
+#' These functions allow you to make interactive legends associated with interactive
+#' layers.
+#' @inheritParams ggplot2::scale_colour_gradient
+#' @param tooltip tooltip associated with keys, one per key, a named vector
+#' @param onclick javascript actions to execute when a key is clicked, one per key, a named vector
+#' @param data_id identifiers to associate with key, one per key, a named vector
+#' @name scale_gradient_interactive
 scale_colour_gradient_interactive <- function(..., low = "#132B43", high = "#56B1F7", space = "Lab",
                                               na.value = "grey50", guide = "colourbar", aesthetics = "colour",
                                               data_id = NULL, onclick = NULL, tooltip = NULL) {
@@ -72,6 +81,7 @@ scale_colour_gradient_interactive <- function(..., low = "#132B43", high = "#56B
 }
 
 #' @export
+#' @rdname scale_gradient_interactive
 scale_fill_gradient_interactive <- function(..., low = "#132B43", high = "#56B1F7", space = "Lab",
                                               na.value = "grey50", guide = "colourbar", aesthetics = "fill",
                                               data_id = NULL, onclick = NULL, tooltip = NULL) {
