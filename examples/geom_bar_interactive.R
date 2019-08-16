1ad4f354670b191770455d449b922d32abf15598
David Gohel <david.gohel@ardata.fr>
2019-08-16 15:32:04 +0200

    add a bar example

--- examples/geom_bar_interactive.R
+++ examples/geom_bar_interactive.R
@@ -24,3 +24,19 @@ p <- ggplot(diamonds, aes(carat)) +
 
 x <- girafe(ggobj = p)
 if( interactive() ) print(x)
+
+# an example with interactive guide ----
+dat <- data.frame(
+  name = c( "Guy", "Ginette", "David", "Cedric", "Frederic" ),
+  gender = c( "Male", "Female", "Male", "Male", "Male" ),
+  height = c(169, 160, 171, 172, 171 ) )
+p <- ggplot(dat, aes( x = name, y = height, fill = gender,
+                      data_id = name ) ) +
+  geom_bar_interactive(stat = "identity") +
+    scale_fill_manual_interactive(
+      values = c(Male = "#0072B2", Female = "#009E73"),
+      data_id = c(Female = "Female", Male = "Male"),
+      tooltip = c(Male = "Male", Female = "Female")
+    )
+x <- girafe(ggobj = p)
+if( interactive() ) print(x)
