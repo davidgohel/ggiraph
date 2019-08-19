95b03d8b97c66333cd2707fc6a2411fdac98e1b3
sigmapi <https://github.com/sigmapi/>
2019-08-19 18:15:49 +0200

    [feat] Handle interactive attributes dynamically (part I)

--- R/utils.R
+++ R/utils.R
@@ -13,14 +13,12 @@ dapply <- ggplot2:::dapply
 #' @importFrom htmltools htmlEscape
 #' @noRd
 encode_cr <- function(x)
-  htmltools::htmlEscape(
-    text = gsub(
-      pattern = "\n",
-      replacement = "<br>",
-      x = x
-    ),
-    attribute = TRUE
-  )
+  htmltools::htmlEscape(text = gsub(
+    pattern = "\n",
+    replacement = "<br>",
+    x = x
+  ),
+  attribute = TRUE)
 
 
 #' @section Geoms:
@@ -37,17 +35,18 @@ encode_cr <- function(x)
 #' @name GeomInteractive
 NULL
 
+INTERACTIVE_ATTR_NAMES <- c("tooltip", "onclick", "data_id")
+INTERACTIVE_ATTR_DEFAULTS <- list(tooltip = NULL,
+                                  onclick = NULL,
+                                  data_id = NULL)
+
 #' Ensures that interactive attributes are characters.
 #' @noRd
 force_interactive_aes_to_char <- function(data) {
-  if (!is.null(data$tooltip) && !is.character(data$tooltip)) {
-    data$tooltip <- as.character(data$tooltip)
-  }
-  if (!is.null(data$onclick) && !is.character(data$onclick)) {
-    data$onclick <- as.character(data$onclick)
-  }
-  if (!is.null(data$data_id) && !is.character(data$data_id)) {
-    data$data_id <- as.character(data$data_id)
+  for (a in INTERACTIVE_ATTR_NAMES) {
+    if (!is.null(data[[a]]) && !is.character(data[[a]])) {
+      data[[a]] <- as.character(data[[a]])
+    }
   }
   data
 }
@@ -55,52 +54,52 @@ force_interactive_aes_to_char <- function(data) {
 #' Copies interactive attributes from one object to the other.
 #' and returns the result
 #' @noRd
-copy_interactive_attrs <- function(src, dest, useList = FALSE, ...) {
-  if (useList == TRUE) {
-    if (!is.null(src$tooltip)) {
-      dest$tooltip <-
-        unlist(mapply(rep, as.character(src$tooltip), ...))
+copy_interactive_attrs <-
+  function(src, dest, useList = FALSE, ...) {
+    if (useList == TRUE) {
+      for (a in INTERACTIVE_ATTR_NAMES) {
+        if (!is.null(src[[a]])) {
+          dest[[a]] <- unlist(mapply(rep, as.character(src[[a]]), ...))
+        }
+      }
+    } else {
+      for (a in INTERACTIVE_ATTR_NAMES) {
+        if (!is.null(src[[a]])) {
+          dest[[a]] <- rep(as.character(src[[a]]), ...)
+        }
+      }
     }
-    if (!is.null(src$onclick)) {
-      dest$onclick <-
-        unlist(mapply(rep, as.character(src$onclick), ...))
-    }
-    if (!is.null(src$data_id)) {
-      dest$data_id <-
-        unlist(mapply(rep, as.character(src$data_id), ...))
-    }
-  } else {
-    if (!is.null(src$tooltip)) {
-      dest$tooltip <- rep(as.character(src$tooltip), ...)
-    }
-    if (!is.null(src$onclick)) {
-      dest$onclick <- rep(as.character(src$onclick), ...)
-    }
-    if (!is.null(src$data_id)) {
-      dest$data_id <- rep(as.character(src$data_id), ...)
-    }
+    dest
   }
-  dest
-}
 
 #' Checks if passed object contains interactive attributes.
 #' @noRd
 has_interactive_attrs <- function(x) {
-  !is.null(x$tooltip) || !is.null(x$onclick) || !is.null(x$data_id)
+  for (a in INTERACTIVE_ATTR_NAMES) {
+    if (!is.null(x[[a]]))
+      return(TRUE)
+  }
+  FALSE
+}
+
+#' Returns the names of the interactive attributes that may exist in an object.
+#' @noRd
+get_interactive_attr_names <- function(x) {
+  intersect(names(x), INTERACTIVE_ATTR_NAMES)
 }
 
 #' Gathers the interactive attributes that may exist in an object.
 #' @noRd
 collect_interactive_attrs <- function(x) {
-  x[intersect(names(x), c("tooltip", "onclick", "data_id"))]
+  x[get_interactive_attr_names(x)]
 }
 
 #' Removes the interactive attributes from an object.
 #' @noRd
 remove_interactive_attrs <- function(x) {
-  x$tooltip <- NULL
-  x$onclick <- NULL
-  x$data_id <- NULL
+  for (a in INTERACTIVE_ATTR_NAMES) {
+    x[[a]] <- NULL
+  }
   x
 }
 
@@ -120,7 +119,7 @@ add_interactive_attrs <- function(gr,
     children_len <- length(gr$children)
     if (is.null(data_len) || data_len == 1) {
       # pass the data as a whole
-      for(i in seq_along(gr$children)){
+      for (i in seq_along(gr$children)) {
         gr$children[[i]] <-
           add_interactive_attrs(
             gr = gr$children[[i]],
@@ -146,9 +145,9 @@ add_interactive_attrs <- function(gr,
     }
     return(gr)
   }
-  gr$tooltip <- data$tooltip
-  gr$onclick <- data$onclick
-  gr$data_id <- data$data_id
+  for (a in INTERACTIVE_ATTR_NAMES) {
+    gr[[a]] <- data[[a]]
+  }
   gr$data_attr <- data_attr
 
   if (is.null(cl)) {
@@ -166,14 +165,7 @@ add_interactive_attrs <- function(gr,
 #' by copying the default aes and appending the interactive attrs.
 #' @noRd
 add_default_interactive_aes <- function(geom = Geom) {
-  append_aes(
-    geom$default_aes,
-    list(
-      tooltip = NULL,
-      onclick = NULL,
-      data_id = NULL
-    )
-  )
+  append_aes(geom$default_aes, INTERACTIVE_ATTR_DEFAULTS)
 }
 
 #' Appends a list of attributes to an aesthetic mapping.
@@ -190,43 +182,36 @@ interactive_attr_toxml <- function(x,
                                    ids = character(0),
                                    rows = NULL,
                                    attr_name = "data-id") {
-  if( length(ids) < 1 )
+  if (length(ids) < 1)
     return(invisible())
 
   if (!is.null(x$data_attr)) {
     attr_name <- x$data_attr
   }
   if (is.null(rows)) {
-    if (!is.null(x$tooltip)) {
-      rows <- seq_along(x$tooltip)
-    } else if (!is.null(x$onclick)) {
-      rows <- seq_along(x$onclick)
-    } else if (!is.null(x$data_id)) {
-      rows <- seq_along(x$data_id)
+    for (a in INTERACTIVE_ATTR_NAMES) {
+      if (is.null(rows) && !is.null(x[[a]])) {
+        rows <- seq_along(x[[a]])
+      }
     }
   }
 
-  if (!is.null(x$tooltip)) {
-    set_attr(
-      ids = as.integer(ids),
-      str = encode_cr(x$tooltip[rows]),
-      attribute = "title"
-    )
+  for (a in INTERACTIVE_ATTR_NAMES) {
+    if (!is.null(x[[a]])) {
+      attrValue <- x[[a]][rows]
+      attrValue <- switch(a,
+                          tooltip = encode_cr(attrValue),
+                          attrValue)
+      attrName <- switch(a,
+                         tooltip = "title",
+                         data_id = attr_name,
+                         a)
+      set_attr(ids = as.integer(ids),
+               str = attrValue,
+               attribute = attrName)
+    }
   }
-  if (!is.null(x$onclick)) {
-    set_attr(
-      ids = as.integer(ids),
-      str = x$onclick[rows],
-      attribute = "onclick"
-    )
-  }
-  if (!is.null(x$data_id)) {
-    set_attr(
-      ids = as.integer(ids),
-      str = x$data_id[rows],
-      attribute = attr_name
-    )
-  }
+  invisible()
 }
 
 #' Returns the arguments for a grob function
@@ -307,10 +292,9 @@ find_interactive_class <- function(geom, env = parent.frame()) {
     }
   } else {
     stop("`geom` must be either a string or a Geom* object, ",
-      "not ",
-      obj_desc(geom),
-      call. = FALSE
-    )
+         "not ",
+         obj_desc(geom),
+         call. = FALSE)
   }
   if (!startsWith(name, "Geom")) {
     name <- paste0("Geom", camelize(name, first = TRUE))
@@ -321,10 +305,9 @@ find_interactive_class <- function(geom, env = parent.frame()) {
   obj <- find_global(name, env = env)
   if (is.null(obj) || !inherits(obj, "Geom")) {
     stop("Can't find interactive geom function called \"",
-      geom,
-      "\"",
-      call. = FALSE
-    )
+         geom,
+         "\"",
+         call. = FALSE)
   } else {
     obj
   }
