fec5ac510710e45eec1dc19f9b0e4e51b8a53a13
David Gohel <david.gohel@ardata.fr>
2019-09-03 17:10:26 +0200

    use latest gdtools

--- R/fonts.R
+++ R/fonts.R
@@ -1,38 +1,18 @@
 r_font_families <- c("sans", "serif", "mono", "symbol")
 
 default_fontname <- function() {
-  if (.Platform$OS.type == "windows") {
-    serif_font <- "Times New Roman"
-  } else {
-    serif_font <- "Times"
-  }
   c(
-    sans = "Arial",
-    serif = serif_font,
-    mono = "Courier",
-    symbol = "Symbol"
+    sans = match_family("sans"),
+    serif = match_family("serif"),
+    mono = match_family("mono"),
+    symbol = match_family("symbol")
   )
 }
 
-match_sys_font <- function(x){
-  if( !( is.character(x) && length(x) == 1 ) ){
-    stop("System fonts must be scalar character vector", call. = FALSE)
-  }
-  matched <- gdtools::match_family(x)
-
-  if (x != matched) {
-    warning(call. = FALSE,
-            "System font `", x, "` not found. ",
-            "Closest match: `", matched, "`")
-  }
-  matched
-}
-
+#' @importFrom gdtools match_family
 validate_fonts <- function(system_fonts = list()) {
   missing_fonts <- setdiff(r_font_families, names(system_fonts) )
-  system_fonts[missing_fonts] <- lapply(default_fontname()[missing_fonts], gdtools::match_family)
-  system_fonts <- lapply(system_fonts, match_sys_font)
+  system_fonts[missing_fonts] <- lapply(default_fontname()[missing_fonts], match_family)
+  system_fonts <- lapply(system_fonts, match_family)
   system_fonts
 }
-
-
