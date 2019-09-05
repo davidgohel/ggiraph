c493d56d45460904c76795840c70c6c6816dcc62
sigmapi <https://github.com/sigmapi/>
2019-09-05 18:36:53 +0200

    [fix] Updated shiny dom test because it was failing

--- tests/testthat/test-dom.R
+++ tests/testthat/test-dom.R
@@ -1,27 +1,19 @@
 context("DOM tests")
 library(xml2)
 
-
 test_that("svg is produced when rendered in html", {
-  skip_if_not_installed("processx")
-  skip_if_not_installed("rdom")
-  library(processx)
-  library(rdom)
+  skip_if_not_installed("shinytest")
+  library(shinytest)
 
-  p <- process$new(R.home('bin/R'),
-                   args = c('-e',
-                            'shiny::runApp(system.file(package = "ggiraph", "examples/shiny/crimes"), port = 1234)') )
-  p$is_alive()
+  app <- ShinyDriver$new(path = system.file(package = "ggiraph", "examples/shiny/crimes"))
+  app$waitFor("plot")
+  dom <- app$getSource()
+  app$stop()
 
-  file <- tempfile(fileext = ".html")
-  rdom::rdom("http://127.0.0.1:1234", filename = file)
-
-  p$kill()
-
-  doc <- read_html(file)
+  doc <- read_html(dom)
   expect_equal(xml_length(xml_find_first(doc, "//svg")), 1)
   svg_dom <- xml_find_first(doc, "//svg")
-  expect_match(xml_attr(svg_dom, "preserveaspectratio"), "xMidYMin")
-  expect_length(xml_find_all(doc, "//svg/g/circle"), 50)
-
+  expect_length(xml_find_all(svg_dom, ".//g/circle"), 50)
+  expect_length(xml_find_all(svg_dom, ".//g/circle[@title]"), 50)
+  expect_length(xml_find_all(svg_dom, ".//g/circle[@data-id]"), 50)
 } )
