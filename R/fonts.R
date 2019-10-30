550ae29846e6a7c084e9209b348325d0ba0eb61a
David Gohel <david.gohel@ardata.fr>
2019-10-30 17:43:24 +0100

    change validate_fonts

--- R/fonts.R
+++ R/fonts.R
@@ -1,18 +1,42 @@
 r_font_families <- c("sans", "serif", "mono", "symbol")
 
+#' @importFrom gdtools font_family_exists
 default_fontname <- function() {
-  c(
-    sans = match_family("sans"),
-    serif = match_family("serif"),
-    mono = match_family("mono"),
-    symbol = match_family("symbol")
-  )
+  def_fonts <- if( Sys.info()["sysname"] == "windows" ){
+    c(
+      sans = "Arial",
+      serif = "Times New Roman",
+      mono = "Courier New",
+      symbol = "Symbol"
+    )
+  } else if( Sys.info()["sysname"] == "darwin" ){
+    c(
+      sans = "Helvetica",
+      serif = "Times",
+      mono = "Courier",
+      symbol = "Symbol"
+    )
+  } else {
+    c(
+      sans = "DejaVu Sans",
+      serif = "DejaVu serif",
+      mono = "DejaVu mono",
+      symbol = "DejaVu Sans"
+    )
+  }
+
+  def_fonts <- def_fonts[unlist(lapply(def_fonts, font_family_exists))]
+  missing_fonts <- setdiff(r_font_families, names(def_fonts) )
+  def_fonts[missing_fonts] <- lapply(def_fonts[missing_fonts], match_family)
+  def_fonts
 }
+
 
 #' @importFrom gdtools match_family
 validate_fonts <- function(system_fonts = list()) {
+  system_fonts <- system_fonts[unlist(lapply(system_fonts, font_family_exists))]
   missing_fonts <- setdiff(r_font_families, names(system_fonts) )
-  system_fonts[missing_fonts] <- lapply(default_fontname()[missing_fonts], match_family)
-  system_fonts <- lapply(system_fonts, match_family)
+  system_fonts[missing_fonts] <- default_fontname()[missing_fonts]
   system_fonts
 }
+
