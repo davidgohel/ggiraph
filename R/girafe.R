c455b9377a5794a2d8f31ba0edaabe8873bf3a28
David Gohel <david.gohel@ardata.fr>
2020-04-04 11:31:01 +0200

    drop readr from imports

--- R/girafe.R
+++ R/girafe.R
@@ -111,7 +111,7 @@ girafe <- function(
   }, finally = dev.off() )
 
   settings <- merge_options(default_opts(), options)
-  x = list( html = readr::read_file(path),
+  x = list( html = paste0(readLines(path, encoding = "UTF-8"), sep = "\n"),
             js = NULL,
             uid = args$canvas_id,
             ratio = width_svg / height_svg,
