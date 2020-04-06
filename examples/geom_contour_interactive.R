ad9541aa1678476b72541499f28f87e12785208b
sigmapi <https://github.com/sigmapi/>
2020-04-06 21:31:58 +0200

    [fix] Fix contour_filled_interactive example for old R version

--- examples/geom_contour_interactive.R
+++ examples/geom_contour_interactive.R
@@ -9,10 +9,12 @@ p <- v + geom_contour_interactive(aes(
 x <- girafe(ggobj = p)
 if (interactive()) print(x)
 
-p <- v + geom_contour_filled_interactive(aes(
-  colour = stat(level),
-  fill = stat(level),
-  tooltip = paste("Level:", stat(level))
-))
-x <- girafe(ggobj = p)
-if (interactive()) print(x)
+if (packageVersion("grid") >= "3.6") {
+  p <- v + geom_contour_filled_interactive(aes(
+    colour = stat(level),
+    fill = stat(level),
+    tooltip = paste("Level:", stat(level))
+  ))
+  x <- girafe(ggobj = p)
+  if (interactive()) print(x)
+}
