390c06096979a32cc7a9467d52d1bb1b5f5a5a48
David Gohel <david.gohel@ardata.fr>
2021-05-11 00:44:26 +0200

    more doc about labeller_interactive
    sitedeploy

--- R/labeller_interactive.R
+++ R/labeller_interactive.R
@@ -9,7 +9,8 @@
 #' for example `theme(strip.text = element_text_interactive())`
 #' @param custom_label_func a function that returns the result
 #' of [label_interactive()].
-#' @param default_labeller a Labeller function, see [label_value()]
+#' @param ... optional additional arguments for function `custom_label_func`.
+#' @param .default a Labeller function, see [label_value()]
 #' @examples
 #' library(ggplot2)
 #' library(ggiraph)
@@ -22,23 +23,45 @@
 #'   theme(strip.text = element_text_interactive())
 #'
 #' x <- girafe(ggobj = gg_jitter)
-#' if( interactive() ) print(x)
+#' if(interactive()) print(x)
+#'
+#'
+#' lab_custom <- function(z, prefix = "label: ") {
+#'   label_interactive(label = z, data_id = z, tooltip = paste0(prefix, z))
+#' }
+#'
+#' gg_jitter <- ggplot(diamonds, aes(x = x)) +
+#'   geom_density() +
+#'   facet_grid(color ~ cut,
+#'     labeller = labeller(
+#'       cut = labeller_interactive(lab_custom, prefix = "cut: "),
+#'       color = labeller_interactive(lab_custom, prefix = "color: ")
+#'     )) +
+#'   theme_light() +
+#'   theme(
+#'     strip.text.x = element_text_interactive(),
+#'     strip.text.y = element_text_interactive()
+#'   )
+#'
+#' x <- girafe(ggobj = gg_jitter, width_svg = 8, height_svg = 5)
+#'
+#' if (interactive()) print(x)
 #' @seealso [id_label()], [label_interactive()]
-labeller_interactive <- function(custom_label_func, default_labeller = ggplot2::label_value) {
+labeller_interactive <- function(custom_label_func=id_label, ..., .default = ggplot2::label_value) {
   # must apply the user custom function for a single label only
   # otherwise the attributes of label_interactive will be dropped
   single_label_func <- function(x) {
     if (is.list(x) || is.matrix(x) || length(x) > 1) {
       lapply(x, single_label_func)
     } else {
-      custom_label_func(x)
+      custom_label_func(x, ...)
     }
   }
 
   # must return a labeller function
   fun <- function(label_df) {
     # call default labeller
-    labels <- default_labeller(label_df)
+    labels <- .default(label_df)
     # call user function
     if (is.function(custom_label_func)) {
       labels <- single_label_func(labels)
