4cdf967c98a0a6dc423310960bb0f7cb3d75a2b5
Panagiotis Skintzos <sigmapi@posteo.net>
2021-11-08 20:45:32 +0100

    [feat] Added geom_count_interactive

---
+++ R/geom_count_interactive.R
@@ -0,0 +1,16 @@
+#' @title Create interactive point counts
+#'
+#' @description
+#' The geometry is based on [geom_bin2d()].
+#' See the documentation for those functions for more details.
+#'
+#' @param ... arguments passed to base function,
+#' plus any of the [interactive_parameters()].
+#' @inheritSection interactive_parameters Details for geom_*_interactive functions
+#' @examples
+#' # add interactive point counts to a ggplot -------
+#' @example examples/geom_count_interactive.R
+#' @seealso [girafe()]
+#' @export
+geom_count_interactive <- function(...)
+  layer_interactive(geom_count, ..., interactive_geom = GeomInteractivePoint)
