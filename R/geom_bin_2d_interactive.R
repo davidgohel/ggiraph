e89c13f520a1d5157f03170a1c569818c577d028
Panagiotis Skintzos <sigmapi@posteo.net>
2021-11-08 20:25:13 +0100

    [feat] Added geom_bin_2d_interactive

---
+++ R/geom_bin_2d_interactive.R
@@ -0,0 +1,21 @@
+#' @title Create interactive heatmaps of 2d bin counts
+#'
+#' @description
+#' The geometry is based on [geom_bin_2d()].
+#' See the documentation for those functions for more details.
+#'
+#' @param ... arguments passed to base function,
+#' plus any of the [interactive_parameters()].
+#' @inheritSection interactive_parameters Details for geom_*_interactive functions
+#' @examples
+#' # add interactive bin2d heatmap to a ggplot -------
+#' @example examples/geom_bin2d_interactive.R
+#' @seealso [girafe()]
+#' @export
+geom_bin_2d_interactive <- function(...)
+  layer_interactive(geom_bin_2d, ..., interactive_geom = GeomInteractiveTile)
+
+#' @export
+#' @rdname geom_bin_2d_interactive
+#' @usage NULL
+geom_bin2d_interactive <- geom_bin_2d_interactive
---
+++ examples/geom_bin2d_interactive.R
@@ -0,0 +1,8 @@
+library(ggplot2)
+library(ggiraph)
+
+p <- ggplot(diamonds, aes(x, y, fill=cut)) + xlim(4, 10) + ylim(4, 10)+
+  geom_bin2d_interactive(aes(tooltip = cut), bins = 30)
+
+x <- girafe(ggobj = p)
+if( interactive() ) print(x)
