e7e8e9f78e965a37ee88af3283bf89b3e0d6f3a0
Panagiotis Skintzos <sigmapi@posteo.net>
2021-11-09 11:25:05 +0100

    Fixes for geom_errorbar_interactive

--- R/geom_errorbar_interactive.R
+++ R/geom_errorbar_interactive.R
@@ -17,43 +17,49 @@ GeomInteractiveErrorbar <- ggproto(
                         panel_params,
                         coord,
                         width = NULL,
-                        flipped_aes = FALSE, 
+                        flipped_aes = FALSE,
                         .ipar = IPAR_NAMES) {
     data <- flip_data(data, flipped_aes)
-    x <- as.vector(rbind(
-      data$xmin,
-      data$xmax,
-      NA,
-      data$x,
-      data$x,
-      NA,
-      data$xmin,
-      data$xmax
-    ))
-    y <- as.vector(rbind(
-      data$ymax,
-      data$ymax,
-      NA,
-      data$ymax,
-      data$ymin,
-      NA,
-      data$ymin,
-      data$ymin
-    ))
-    box <- new_data_frame(
-      list(
-        x = x,
-        y = y,
-        colour = rep(data$colour, each = 8),
-        alpha = rep(data$alpha, each = 8),
-        size = rep(data$size, each = 8),
-        linetype = rep(data$linetype, each = 8),
-        group = rep(1:(nrow(data)), each = 8),
-        row.names = 1:(nrow(data) * 8)
+
+    # because of errors when there are NA's or in facets when we use fill,
+    # it's safer to draw each bar separately
+    grobs <- lapply(seq_len(nrow(data)), function(i) {
+      row <- data[i, , drop = FALSE]
+
+      box <- new_data_frame(
+        list(
+          x = as.vector(rbind(
+            row$xmin,
+            row$xmax,
+            NA,
+            row$x,
+            row$x,
+            NA,
+            row$xmin,
+            row$xmax
+          )),
+          y = as.vector(rbind(
+            row$ymax,
+            row$ymax,
+            NA,
+            row$ymax,
+            row$ymin,
+            NA,
+            row$ymin,
+            row$ymin
+          )),
+          colour = rep(row$colour, each = 8),
+          alpha = rep(row$alpha, each = 8),
+          size = rep(row$size, each = 8),
+          linetype = rep(row$linetype, each = 8),
+          group = rep(1:(nrow(row)), each = 8),
+          row.names = 1:(nrow(row) * 8)
+        )
       )
-    )
-    box <- copy_interactive_attrs(data, box, each = 8, ipar = .ipar)
-    box <- flip_data(box, flipped_aes)
-    GeomInteractivePath$draw_panel(box, panel_params, coord, .ipar = .ipar)
+      box <- copy_interactive_attrs(row, box, each = 8, ipar = .ipar)
+      box <- flip_data(box, flipped_aes)
+      GeomInteractivePath$draw_panel(box, panel_params, coord, .ipar = .ipar)
+    })
+    grobTree(do.call(gList, grobs))
   }
 )
--- R/geom_errorbarh_interactive.R
+++ R/geom_errorbarh_interactive.R
@@ -25,43 +25,47 @@ GeomInteractiveErrorbarh <- ggproto(
   default_aes = add_default_interactive_aes(GeomErrorbarh),
   parameters = interactive_geom_parameters,
   draw_key = interactive_geom_draw_key,
-  draw_panel = function(data, panel_params, coord, height = NULL, 
+  draw_panel = function(data, panel_params, coord, height = NULL,
                         .ipar = IPAR_NAMES) {
-    box <- new_data_frame(
-      list(
-        x = as.vector(
-          rbind(
-            data$xmax,
-            data$xmax,
+
+    # because of errors when there are NA's or in facets when we use fill,
+    # it's safer to draw each bar separately
+    grobs <- lapply(seq_len(nrow(data)), function(i) {
+      row <- data[i, , drop = FALSE]
+
+      box <- new_data_frame(
+        list(
+          x = as.vector(rbind(
+            row$xmax,
+            row$xmax,
             NA,
-            data$xmax,
-            data$xmin,
+            row$xmax,
+            row$xmin,
             NA,
-            data$xmin,
-            data$xmin
-          )
-        ),
-        y = as.vector(
-          rbind(
-            data$ymin,
-            data$ymax,
+            row$xmin,
+            row$xmin
+          )),
+          y = as.vector(rbind(
+            row$ymin,
+            row$ymax,
             NA,
-            data$y,
-            data$y,
+            row$y,
+            row$y,
             NA,
-            data$ymin,
-            data$ymax
-          )
-        ),
-        colour = rep(data$colour, each = 8),
-        alpha = rep(data$alpha, each = 8),
-        size = rep(data$size, each = 8),
-        linetype = rep(data$linetype, each = 8),
-        group = rep(1:(nrow(data)), each = 8),
-        row.names = 1:(nrow(data) * 8)
+            row$ymin,
+            row$ymax
+          )),
+          colour = rep(row$colour, each = 8),
+          alpha = rep(row$alpha, each = 8),
+          size = rep(row$size, each = 8),
+          linetype = rep(row$linetype, each = 8),
+          group = rep(1:(nrow(row)), each = 8),
+          row.names = 1:(nrow(row) * 8)
+        )
       )
-    )
-    box <- copy_interactive_attrs(data, box, each = 8, ipar = .ipar)
-    GeomInteractivePath$draw_panel(box, panel_params, coord, .ipar = .ipar)
+      box <- copy_interactive_attrs(row, box, each = 8, ipar = .ipar)
+      GeomInteractivePath$draw_panel(box, panel_params, coord, .ipar = .ipar)
+    })
+    grobTree(do.call(gList, grobs))
   }
 )
