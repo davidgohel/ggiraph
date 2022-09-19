9c34ab59308460e9519268c08c84aca34ad79070
Panagiotis Skintzos <sigmapi@posteo.net>
2022-09-19 14:48:07 +0200

    fix/refact: fixed error in extracting line coords and moved generic functions out of main class

--- srcjs/nearest.js
+++ srcjs/nearest.js
@@ -65,17 +65,17 @@ export default class NearestHandler {
       tonode = node.getCTM();
       // get the bounding box of the node and apply transforms
       box = node.getBBox();
-      boxpoints = this.extractPoints(box).map((p) =>
+      boxpoints = extractPoints(box).map((p) =>
         p.matrixTransform(tonode).matrixTransform(tosvg)
       );
       // add the box extent to the spatial index
-      extent = this.getExtent(boxpoints);
+      extent = getExtent(boxpoints);
       this.spatialIndex.add(extent.minx, extent.miny, extent.maxx, extent.maxy);
 
       if (this.debug) {
-        //this.drawObject(boxp, rootNode);
-        this.drawObject(
-          this.getExtentRect(extent, svgNode),
+        //drawObject(boxp, rootNode);
+        drawObject(
+          getExtentRect(extent, svgNode),
           svgNode,
           node.id + '_extent'
         );
@@ -90,7 +90,7 @@ export default class NearestHandler {
         node.gpoints = boxpoints;
         node.gpoints.shape = 'polygon';
       } else {
-        node.gpoints = this.extractPoints(node).map((p) =>
+        node.gpoints = extractPoints(node).map((p) =>
           p.matrixTransform(tonode).matrixTransform(tosvg)
         );
         if (node instanceof SVGCircleElement) {
@@ -104,9 +104,9 @@ export default class NearestHandler {
         }
       }
       if (this.debug) {
-        this.drawObject(node.gpoints, svgNode, node.id + '_points');
+        drawObject(node.gpoints, svgNode, node.id + '_points');
       }
-      node.grect = this.getExtentRect(extent, svgNode);
+      node.grect = getExtentRect(extent, svgNode);
     }, this);
 
     // build spatial index
@@ -116,11 +116,11 @@ export default class NearestHandler {
     // store rootNode box
     box = rootNode.getBBox();
     tonode = rootNode.getCTM();
-    rootNode.gpoints = this.extractPoints(box).map((p) =>
+    rootNode.gpoints = extractPoints(box).map((p) =>
       p.matrixTransform(tonode).matrixTransform(tosvg)
     );
     if (this.debug) {
-      this.drawObject(
+      drawObject(
         rootNode.gpoints,
         svgNode,
         rootNode.id + '_rootpoints',
@@ -155,8 +155,7 @@ export default class NearestHandler {
       }
       // get mouse position in svg coord space
       mousePos = mousePos.matrixTransform(target.getScreenCTM().inverse());
-      if (this.debug)
-        this.drawObject([mousePos], svgNode, svgNode.id + '_mouse');
+      if (this.debug) drawObject([mousePos], svgNode, svgNode.id + '_mouse');
 
       // get maximum 10 nearest elements from the index
       let neighbors = this.spatialIndex
@@ -177,16 +176,10 @@ export default class NearestHandler {
         let zm = d3.zoomTransform(rootNode);
         zm = new DOMMatrix([zm.k, 0, 0, zm.k, zm.x, zm.y]).inverse();
         viewpoints = rootNode.gpoints.map((p) => p.matrixTransform(zm));
-        view = this.getRect(viewpoints, svgNode);
+        view = getRect(viewpoints, svgNode);
 
         if (this.debug)
-          this.drawObject(
-            view,
-            svgNode,
-            rootNode.id + '_view',
-            'none',
-            '#00aa00'
-          );
+          drawObject(view, svgNode, rootNode.id + '_view', 'none', '#00aa00');
 
         // keep the nodes with their bbox is inside the current view
         neighbors = neighbors.filter((node) =>
@@ -198,7 +191,7 @@ export default class NearestHandler {
       if (neighbors.length && svgNode.checkIntersection) {
         const toroot = rootNode.getCTM();
         viewpoints = viewpoints.map((p) => p.matrixTransform(toroot));
-        view = this.getRect(viewpoints, svgNode);
+        view = getRect(viewpoints, svgNode);
         neighbors = neighbors.filter((node) =>
           svgNode.checkIntersection(node, view)
         );
@@ -233,147 +226,145 @@ export default class NearestHandler {
     }
     return null;
   }
+}
 
-  extractPoints(obj) {
-    let points, p, plist, i, d, m;
-    const rePathCoords = new RegExp('([\\d\\.]+) ([\\d\\.]+)', 'g');
-    if (obj instanceof SVGRect || obj instanceof DOMRect) {
-      return [
-        new DOMPoint(obj.x, obj.y),
-        new DOMPoint(obj.x + obj.width, obj.y),
-        new DOMPoint(obj.x + obj.width, obj.y + obj.height),
-        new DOMPoint(obj.x, obj.y + obj.height)
-      ];
-    } else if (
-      obj instanceof SVGRectElement ||
-      obj instanceof SVGImageElement
-    ) {
-      return [
-        new DOMPoint(obj.x.baseVal.value, obj.y.baseVal.value),
-        new DOMPoint(
-          obj.x.baseVal.value + obj.width.baseVal.value,
-          obj.y.baseVal.value
-        ),
-        new DOMPoint(
-          obj.x.baseVal.value + obj.width.baseVal.value,
-          obj.y.baseVal.value + obj.height.baseVal.value
-        ),
-        new DOMPoint(
-          obj.x.baseVal.value,
-          obj.y.baseVal.value + obj.height.baseVal.value
-        )
-      ];
-    } else if (obj instanceof SVGCircleElement) {
-      p = new DOMPoint(obj.cx.baseVal.value, obj.cy.baseVal.value);
-      p.r = obj.r.baseVal.value;
-      return [p];
-    } else if (obj instanceof SVGLineElement) {
-      points = [
-        new DOMPoint(obj.x1.baseVal.value, obj.y1.baseVal.value),
-        new DOMPoint(obj.x2.baseVal.value, obj.y2.baseVal.value)
-      ];
-      points.shape = 'polyline';
-    } else if (
-      obj instanceof SVGPolylineElement ||
-      obj instanceof SVGPolygonElement
-    ) {
-      points = [];
-      plist = obj.points;
-      for (i = 0; i < plist.length; i++) {
-        p = plist.getItem(i);
-        if (!(p instanceof DOMPoint)) {
-          p = new DOMPoint(p.x, p.y);
-        }
-        points.push(p);
+function extractPoints(obj) {
+  let points, p, plist, i, d, m;
+  const rePathCoords = new RegExp('([\\d\\.]+) ([\\d\\.]+)', 'g');
+  if (obj instanceof SVGRect || obj instanceof DOMRect) {
+    return [
+      new DOMPoint(obj.x, obj.y),
+      new DOMPoint(obj.x + obj.width, obj.y),
+      new DOMPoint(obj.x + obj.width, obj.y + obj.height),
+      new DOMPoint(obj.x, obj.y + obj.height)
+    ];
+  } else if (obj instanceof SVGRectElement || obj instanceof SVGImageElement) {
+    return [
+      new DOMPoint(obj.x.baseVal.value, obj.y.baseVal.value),
+      new DOMPoint(
+        obj.x.baseVal.value + obj.width.baseVal.value,
+        obj.y.baseVal.value
+      ),
+      new DOMPoint(
+        obj.x.baseVal.value + obj.width.baseVal.value,
+        obj.y.baseVal.value + obj.height.baseVal.value
+      ),
+      new DOMPoint(
+        obj.x.baseVal.value,
+        obj.y.baseVal.value + obj.height.baseVal.value
+      )
+    ];
+  } else if (obj instanceof SVGCircleElement) {
+    p = new DOMPoint(obj.cx.baseVal.value, obj.cy.baseVal.value);
+    p.r = obj.r.baseVal.value;
+    return [p];
+  } else if (obj instanceof SVGLineElement) {
+    points = [
+      new DOMPoint(obj.x1.baseVal.value, obj.y1.baseVal.value),
+      new DOMPoint(obj.x2.baseVal.value, obj.y2.baseVal.value)
+    ];
+    points.shape = 'polyline';
+    return points;
+  } else if (
+    obj instanceof SVGPolylineElement ||
+    obj instanceof SVGPolygonElement
+  ) {
+    points = [];
+    plist = obj.points;
+    for (i = 0; i < plist.length; i++) {
+      p = plist.getItem(i);
+      if (!(p instanceof DOMPoint)) {
+        p = new DOMPoint(p.x, p.y);
       }
-      points.shape = obj instanceof SVGPolylineElement ? 'polyline' : 'polygon';
-      return points;
-    } else if (obj instanceof SVGPathElement) {
-      points = [];
-      d = obj.getAttribute('d');
-      while ((m = rePathCoords.exec(d)) !== null) {
-        points.push(new DOMPoint(parseFloat(m[1]), parseFloat(m[2])));
-      }
-      return points;
-    } else {
-      throw (
-        'Error in extractPoints: unimplemented object type: ' +
-        Object.prototype.toString.call(obj)
-      );
+      points.push(p);
     }
-  }
-
-  getExtent(points) {
-    const xx = points.map((p) => p.x);
-    const yy = points.map((p) => p.y);
-    return {
-      minx: Math.min.apply(null, xx),
-      maxx: Math.max.apply(null, xx),
-      miny: Math.min.apply(null, yy),
-      maxy: Math.max.apply(null, yy)
-    };
-  }
-
-  getExtentRect(extent, svgNode) {
-    const rect = svgNode.createSVGRect();
-    rect.x = extent.minx;
-    rect.y = extent.miny;
-    rect.width = extent.maxx - extent.minx;
-    rect.height = extent.maxy - extent.miny;
-    return rect;
-  }
-
-  getRect(points, svgNode) {
-    return this.getExtentRect(this.getExtent(points), svgNode);
-  }
-
-  buffer(rect, buffer) {
-    rect.x -= buffer;
-    rect.y -= buffer;
-    rect.width += buffer * 2;
-    rect.height += buffer * 2;
-    return rect;
-  }
-
-  drawObject(obj, parent, id, fill = '#ff000033', stroke = '#ff0000') {
-    const svgns = 'http://www.w3.org/2000/svg';
-    let el = document.getElementById(id);
-    let name;
-    if (Array.isArray(obj) && obj.length === 1) {
-      obj = obj[0];
+    points.shape = obj instanceof SVGPolylineElement ? 'polyline' : 'polygon';
+    return points;
+  } else if (obj instanceof SVGPathElement) {
+    points = [];
+    d = obj.getAttribute('d');
+    while ((m = rePathCoords.exec(d)) !== null) {
+      points.push(new DOMPoint(parseFloat(m[1]), parseFloat(m[2])));
     }
-    if (!el) {
-      name = obj.shape || 'polygon';
-      if (obj instanceof SVGPoint || obj instanceof DOMPoint) {
-        name = 'circle';
-      } else if (obj instanceof SVGRect || obj instanceof DOMRect) {
-        name = 'rect';
-      }
-      el = document.createElementNS(svgns, name);
-      el.setAttribute('id', id);
-      if (name !== 'polyline') el.setAttribute('fill', fill);
-      el.setAttribute('stroke', stroke);
-      el.setAttribute('style', 'pointer-events: none;');
-      parent.appendChild(el);
-    } else {
-      name = el.localName;
-    }
-    if (name === 'circle') {
-      el.setAttribute('cx', obj.x);
-      el.setAttribute('cy', obj.y);
-      el.setAttribute('r', obj.r || '3pt');
-    } else if (name === 'rect') {
-      el.setAttribute('x', obj.x);
-      el.setAttribute('y', obj.y);
-      el.setAttribute('width', obj.width);
-      el.setAttribute('height', obj.height);
-    } else if (name === 'polygon' || name === 'polyline') {
-      let str = '';
-      obj.forEach(function (p) {
-        str += p.x + ' ' + p.y + ' ';
-      });
-      el.setAttribute('points', str);
-    }
-    return el;
+    return points;
+  } else {
+    throw (
+      'Error in extractPoints: unimplemented object type: ' +
+      Object.prototype.toString.call(obj)
+    );
   }
+}
+
+function getExtent(points) {
+  const xx = points.map((p) => p.x);
+  const yy = points.map((p) => p.y);
+  return {
+    minx: Math.min.apply(null, xx),
+    maxx: Math.max.apply(null, xx),
+    miny: Math.min.apply(null, yy),
+    maxy: Math.max.apply(null, yy)
+  };
+}
+
+function getExtentRect(extent, svgNode) {
+  const rect = svgNode.createSVGRect();
+  rect.x = extent.minx;
+  rect.y = extent.miny;
+  rect.width = extent.maxx - extent.minx;
+  rect.height = extent.maxy - extent.miny;
+  return rect;
+}
+
+function getRect(points, svgNode) {
+  return getExtentRect(getExtent(points), svgNode);
+}
+
+function buffer(rect, buffer) {
+  rect.x -= buffer;
+  rect.y -= buffer;
+  rect.width += buffer * 2;
+  rect.height += buffer * 2;
+  return rect;
+}
+
+function drawObject(obj, parent, id, fill = '#ff000033', stroke = '#ff0000') {
+  const svgns = 'http://www.w3.org/2000/svg';
+  let el = document.getElementById(id);
+  let name;
+  if (Array.isArray(obj) && obj.length === 1) {
+    obj = obj[0];
+  }
+  if (!el) {
+    name = obj.shape || 'polygon';
+    if (obj instanceof SVGPoint || obj instanceof DOMPoint) {
+      name = 'circle';
+    } else if (obj instanceof SVGRect || obj instanceof DOMRect) {
+      name = 'rect';
+    }
+    el = document.createElementNS(svgns, name);
+    el.setAttribute('id', id);
+    if (name !== 'polyline') el.setAttribute('fill', fill);
+    el.setAttribute('stroke', stroke);
+    el.setAttribute('style', 'pointer-events: none;');
+    parent.appendChild(el);
+  } else {
+    name = el.localName;
+  }
+  if (name === 'circle') {
+    el.setAttribute('cx', obj.x);
+    el.setAttribute('cy', obj.y);
+    el.setAttribute('r', obj.r || '3pt');
+  } else if (name === 'rect') {
+    el.setAttribute('x', obj.x);
+    el.setAttribute('y', obj.y);
+    el.setAttribute('width', obj.width);
+    el.setAttribute('height', obj.height);
+  } else if (name === 'polygon' || name === 'polyline') {
+    let str = '';
+    obj.forEach(function (p) {
+      str += p.x + ' ' + p.y + ' ';
+    });
+    el.setAttribute('points', str);
+  }
+  return el;
 }
