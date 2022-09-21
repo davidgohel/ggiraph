9defd46e8655a90c5cfd21e3cd3a920526bd8690
Panagiotis Skintzos <sigmapi@posteo.net>
2022-09-21 09:32:55 +0200

    fix: add flatbush to externals

--- tools/packagejs/webpack.config.js
+++ tools/packagejs/webpack.config.js
@@ -126,7 +126,7 @@ module.exports = (env, argv) => {
       library: PACKAGE.name,
       libraryTarget: 'umd'
     },
-    externals: ['d3'],
+    externals: ['d3','flatbush'],
     module: {
       rules: [
         {
@@ -169,7 +169,7 @@ module.exports = (env, argv) => {
       modules: modulePaths
     },
     plugins: [
-      // copy save-svg-as-png and minimize it
+      // copy libs
       new CopyPlugin({
         patterns: [
           {
@@ -178,29 +178,17 @@ module.exports = (env, argv) => {
             transform(content, path) {
               return UglifyJS.minify(content.toString()).code.toString();
             }
-          }
-        ]
-      }),
-
-      // copy flatbush and minimize it
-      new CopyPlugin({
-        patterns: [
+          },
+          {
+            from: libInfos['d3-lasso'].srcJs,
+            to: getLibDestJsFilename('d3-lasso')
+          },
           {
             from: libInfos['flatbush'].srcJs,
             to: getLibDestJsFilename('flatbush'),
             transform(content, path) {
               return UglifyJS.minify(content.toString()).code.toString();
             }
-          }
-        ]
-      }),
-
-      // copy d3-lasso
-      new CopyPlugin({
-        patterns: [
-          {
-            from: libInfos['d3-lasso'].srcJs,
-            to: getLibDestJsFilename('d3-lasso')
           }
         ]
       }),
@@ -228,6 +216,7 @@ module.exports = (env, argv) => {
               'd3-bundle',
               'd3-lasso',
               'save-svg-as-png',
+              'flatbush',
               PACKAGE.name
             ].map((x) => {
               const i = libInfos[x];
